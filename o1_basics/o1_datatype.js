f075cb328b66b7e7098fef9239996787ae627077
sonja-samuel <sonjasamuel14@gmail.com>
2025-01-27 07:38:38 +0530

    conversion of datatype

---
+++ o1_basics/o1_datatype.js
@@ -0,0 +1,10 @@
+let score = "33"
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// conversion
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
